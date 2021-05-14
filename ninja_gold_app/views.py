c0c3ae075c2bf4660bca39b605144c1c42cc8a37
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-14 15:29:00 -0500

    played around... rupee img removed as i was trying to get it to be background but couldnt figure it out

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -5,42 +5,47 @@ from django.forms import HiddenInput
 
 from datetime import datetime
 
-today = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+today = datetime.now().strftime("%m/%d/%Y %I:%M%p")
 
 def index(request):
-    if 'rupees' not in request.session:
+    if 'rupees' not in request.session or 'activity_log' not in request.session:
         request.session['rupees'] = 0
-    if 'activity_log' not in request.session:
-        request.session['activity_log'] = ''
+        request.session['activity_log'] = []
     
     return render(request,'index.html')
 
 def farm(request):
     farm_add = rand.randrange(10, 20)
+    farm_message = f"Earned {str(farm_add)} rupees from the farm! - {str(today)}"
     request.session['rupees'] += farm_add
-    request.session['activity_log'] += f"Earned {str(farm_add)} rupees from the farm! - {str(today)}   "
-# <p>Earned {str(request.session['rupees'])} rupees from the farm! - {str(today)}</p>"
+    request.session['activity_log'].append({'message': farm_message})
+    # <p>Earned {str(request.session['rupees'])} rupees from the farm! - {str(today)}</p>"
     return redirect('/')
 
 def cave(request):
     cave_add = rand.randrange(5, 10)
+    cave_message = f"Earned {str(cave_add)} rupees from the cave! - {str(today)}"
     request.session['rupees'] += cave_add
-    request.session['activity_log'] += f"Earned {str(cave_add)} rupees from the cave! - {str(today)}   "
+    request.session['activity_log'].append({'message': cave_message})
     return redirect('/')
 
 def house(request):
     house_add = rand.randrange(2, 5)
+    house_message = f"Earned {str(house_add)} rupees from the house! - {str(today)}"
     request.session['rupees'] += house_add
-    request.session['activity_log'] += f"Earned {str(house_add)} rupees from the house! - {str(today)}  "
+    request.session['activity_log'].append({'message': house_message})
+    print(request.session['activity_log'])
     return redirect('/')
 
 def casino(request):
     casino_change = rand.randrange(-50, 50)
     request.session['rupees'] += casino_change
     if casino_change < 0:
-        request.session['activity_log'] += f"Entered a casino and lost {str(casino_change)} rupees. Ouch! That might teach you to gamble. - {str(today)}    "
+        casino_message = f"Entered a casino and lost {str(casino_change)} rupees. Ouch! That might teach you to gamble. - {str(today)}"
+        request.session['activity_log'].append({'message': casino_message})
     else:
-        request.session['activity_log'] += f"It is your lucky day. You entered a casino and won {str(casino_change)} rupees! - {str(today)}     "
+        casino_message = f"It is your lucky day. You entered a casino and won {str(casino_change)} rupees! - {str(today)}"
+        request.session['activity_log'].append({'message': casino_message})
     return redirect('/')
 
 def process_rupees(request):
